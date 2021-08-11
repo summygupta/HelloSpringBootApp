5406fdcf4776f272b6bac0ae937313af314f9df1
Summy Gupta <guptasummy21.1994@gmail.com>
2021-08-11 23:32:04 +0530

    initial-commit

---
+++ helloWorld/src/test/java/com/example/springboot/helloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.helloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
